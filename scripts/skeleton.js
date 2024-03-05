3768a943f52f8574fb0d5f433129bae903b7f643
Corey Buchan <xouwan021592@gmail.com>
2024-03-05 14:21:18 -0800

    Add in Skeleton JS to use skeleton components

---
+++ scripts/skeleton.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton() {
+  console.log($('#footerPlaceholder').load('./text/footer.html'))
+  console.log($('#headerPlaceholder').load('./text/header.html'))
+}
+loadSkeleton();
