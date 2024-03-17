ec2a3361f2a356b4972cffb3481b902f805780dc
Corey Buchan <xouwan021592@gmail.com>
2024-03-16 18:37:40 -0700

    Made home button redirect based on if user is logged in or not. Moved homeButton and SignoutButton code to header.js

---
+++ scripts/header.js
@@ -0,0 +1,27 @@
+function onClickHome(){
+  if (app.auth().currentUser == null) {
+    location.pathname = '/index.html';
+  } else {
+    location.pathname = '/main.html';
+  }
+}
+
+function setupLogoutButton(){
+  //Logs the user out and sends them to the index when they click 'logout'.
+  logoutButton.addEventListener('click', function(e) {
+    logout();
+    location.pathname = '/index.html';
+  });
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      logoutButton.style.visibility = 'visible';
+    } else {
+      console.log("No user detected.")
+      logoutButton.style.visibility = 'hidden';
+    }
+  });
+}
+
+document.getElementById("homeButton").addEventListener('click', onClickHome);
+setupLogoutButton();
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -4,24 +4,7 @@
 //---------------------------------------------------
 function loadSkeleton() {
   $('#footerPlaceholder').load('./text/footer.html');
-  $('#headerPlaceholder').load('./text/header.html', setupLogoutButton)
-}
-
-function setupLogoutButton(){
-  //Logs the user out and sends them to the index when they click 'logout'.
-  logoutButton.addEventListener('click', function(e) {
-    logout();
-    location.pathname = '/index.html';
-  });
-
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      logoutButton.style.visibility = 'visible';
-    } else {
-      console.log("No user detected.")
-      logoutButton.style.visibility = 'hidden';
-    }
-  });
+  $('#headerPlaceholder').load('./text/header.html')
 }
 
 loadSkeleton();
