cb58d47db72f7ff301209bb2236da876b4b79c58
Corey Buchan <xouwan021592@gmail.com>
2024-03-16 18:25:20 -0700

    Remove placeholder images and header code. Add logout button and script

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,25 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton() {
-  console.log($('#footerPlaceholder').load('./text/footer.html'))
-  console.log($('#headerPlaceholder').load('./text/header.html'))
+  $('#footerPlaceholder').load('./text/footer.html');
+  $('#headerPlaceholder').load('./text/header.html', setupLogoutButton)
 }
+
+function setupLogoutButton(){
+  //Logs the user out and sends them to the index when they click 'logout'.
+  logoutButton.addEventListener('click', function(e) {
+    logout();
+    location.pathname = '/index.html';
+  });
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      logoutButton.style.visibility = 'visible';
+    } else {
+      console.log("No user detected.")
+      logoutButton.style.visibility = 'hidden';
+    }
+  });
+}
+
 loadSkeleton();
