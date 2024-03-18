752519d8be6c37f4054eb8a611c4a2cea26c1c9e
Corey Buchan <xouwan021592@gmail.com>
2024-03-18 08:25:34 -0700

    Add watchPosition placeholder. Add search placeholder

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,22 @@ $('#mapArea').load('./images/map/SVG/VectorTrace.svg', function(){
   document.getElementById('Layer_2').childNodes.forEach(child => {
     if (child.nodeName == 'defs'){
       child.remove();
-    }
-    console.log(child.nodeName);
-    
+    }    
   })
 });
+
+navigator.geolocation.watchPosition(position => {
+  console.log(position);
+});
+
+/**
+ * 
+ * @param {String} term 
+ */
+function search(term){
+    if (isNaN(Number(term))) {
+      //We search by building or room name/type
+  } else {
+      //We search by room number.
+  }
+}
