9dbba93d6ec618fe3d854c36bcccaca249f2e321
Alex He <donut11one@gmail.com>
2024-03-26 14:33:07 -0700

    NO MORE CRASHING FOR pfNode.js now SHOULD go through the nodes and pick fastest route

--- scripts/pfNode.js
+++ scripts/pfNode.js
@@ -2,81 +2,53 @@ class pfNode{
     constructor(data, f , g , h , x, y){
         this.f = f;
         this.g = g;
-        this.h = h;
+        this.h;
         this.visited = false;
         this.closed = false;
         this.parent = null;
+        
         this.pos =[x,y]
 
     
         this.data=data;
-        this.next = null;
+        this.right =  null;
+        this.left = null;
     }   
 }
-class List{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    add(data, f , g , h , cost){
-        const newNode = new pfNode(data, f , g , h , cost);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-        } else{
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
-    printAll(){
-        let current = this.head;
-        while(current){
-            console.log(current.data);
-            console.log("Cost to get to current node + heuristics: " + current.f);
-            console.log("Cost to get to current node: " + current.g);
-            console.log("Heuristics: " + current.h);
-            console.log("Position: " + current.pos);
-
-            current = current.next;
-        }
-    }
-}
-
-
-
 var astar = {
-    init: function(list) {
-        let newlist = new List();
-        for(var x = 0; x < list.length ; x++){   
-            newlist.add(list[x][0] , list[x][1] , list[x][2], list[x][3]);
-            
-        }
-        return newlist;
-        newlist.printAll();
+  init: function(list) {
+    let grid = [[1,2],
+                [3,4],
+                [5,6]];
+    let counter = 0; 
+    for(var x = 0; x < 3 ; x++){
+      for(var i = 0 ; i < 2 ; i++){
+        grid[x][i] = (new pfNode(list[counter][0] , list[counter][1] , list[counter][2], list[counter][3],list[counter][4][0], list[counter][4][1]));
+        counter++;
+      }
+    }
+      return grid;
     },
     //Search function of the nearby nodes.
     search: function(grid) {
-        var list = astar.init(grid);
-        start = list.start;
-        end = list.end; 
-     
-        var openList = grid;
-        var closedList = new List;
-        openList.push(start);
-     
-        while(openList.length > 0) {
-     
-          // Grab the lowest f(x) to process next
-          var lowInd = 0;
-          for(var i= 0; i<openList.length; i++) {
-            if(openList.f < openList.f) { 
-                lowInd = i; 
-            }
+      grid = astar.init(grid);
+      start = grid[0][0];
+      end = grid[grid.length-1][1];
+      var openList   = [];
+      var closedList = [];
+      openList.push(start);
+      
+      
+      while(openList != null) {
+        console.log(openList);
+        // Grab the lowest f(x) to process next
+        var lowInd = 0;
+        for(var i=0; i<openList.length; i++) {
+          if(openList[i].f < openList[lowInd].f) {
+            lowInd = i;
           }
-          var currentNode = list.head;
+        }
+        var currentNode = openList[lowInd];
      
           // End case -- result has been found, return the traced path
           if(currentNode == end ) {
@@ -89,39 +61,38 @@ var astar = {
             return ret.reverse();
           }
           // Normal case -- move currentNode from open to closed, process each of its neighbors
-          openList.removeGraphNode(currentNode);
+          openList = astar.removeGraphNode(currentNode, openList);
+          console.log(openList);
+
           closedList.push(currentNode);
+          
           var neighbors = astar.neighbors(grid, currentNode);
-     
+
           for(var i= 0; i<neighbors.length;i++) {
             var neighbor = neighbors[i];
-            if(closedList.findGraphNode(neighbor) || neighbor.isWall()) {
+            if(astar.findGraphNode(neighbor, closedList)) {
               // not a valid node to process, skip to next neighbor
               continue;
             }
      
             // g score is the shortest distance from start to current node, we need to check if
             //   the path we have arrived at this neighbor is the shortest one we have seen yet
-            var gScore = currentNode.g + 1; // 1 is the distance from a node to it's neighbor
+            var gScore = currentNode.g + 1  ; // 1 is the distance from a node to it's neighbor
             var gScoreIsBest = false;
-     
-     
-            if(!openList.findGraphNode(neighbor)) {
-              // This the the first time we have arrived at this node, it must be the best
-              // Also, we need to take the h (heuristic) score since we haven't done so yet
-     
-              gScoreIsBest = true;
-              neighbor.h = astar.heuristic(neighbor.pos, end.pos);
-              openList.push(neighbor);
-            }
-            else if(gScore < neighbor.g) {
-              // We have already seen the node, but last time it had a worse g (distance from start)
+            // if(!astar.findGraphNode(neighbor, openList)) {
+            //   // This the the first time we have arrived at this node, it must be the best
+            //   // Also, we need to take the h (heuristic) score since we haven't done so yet
+            //   gScoreIsBest = true;
+              
+            //   neighbor.h = astar.heuristic(neighbor.pos, end.pos);
+
+            //   openList.push(neighbor);
+            // }
+            if(gScore < neighbor.g) {
               gScoreIsBest = true;
             }
      
             if(gScoreIsBest) {
-              // Found an optimal (so far) path to this node.   Store info on how we got here and
-              //  just how good it really is...
               neighbor.parent = currentNode;
               neighbor.g = gScore;
               neighbor.f = neighbor.g + neighbor.h;
@@ -135,15 +106,24 @@ var astar = {
       },
       heuristic: function(pos0, pos1) {
         // This is the Manhattan distance
-        var d1 = Math.abs (pos1.x - pos0.x);
-        var d2 = Math.abs (pos1.y - pos0.y);
+        console.log(pos1);
+        var d1 = Math.abs (pos1[0] - pos0[0]);
+        var d2 = Math.abs (pos1[1] - pos0[1]);
         return d1 + d2;
       },
       neighbors: function(grid, node) {
         var ret = [];
-        var x = node.pos.x;
-        var y = node.pos.y;
-     
+        var x = 1;
+        var y = 1;
+        
+        for(let i = 0 ; i < grid.length ; i++){
+          for(let j = 0; j < grid[i].length; j++){
+            if(grid[i][j] == node){
+              x=i;
+              y=j;
+            }
+          }
+        }     
         if(grid[x-1] && grid[x-1][y]) {
           ret.push(grid[x-1][y]);
         }
@@ -157,6 +137,31 @@ var astar = {
           ret.push(grid[x][y+1]);
         }
         return ret;
+      },
+
+      findGraphNode: function(neighbour, list){
+          for(let i  = 0 ; i < list.length; i++){
+            if(neighbour == list[i]){
+              return true;
+            }
+          }
+          return false;
+      },
+
+      removeGraphNode: function(currentNode, openList){
+        for(let i = 0; i<openList.length; i++){
+          if(openList == currentNode){
+
+
+            openList.splice(i,1);
+          }
+        }
+
+      },
+      showNode: function(x,y){
+
+
       }
     };
-astar.search([["Classroom 1",6,0,6,[0,0]],["Outside",6,1,5, [0,1]],["Outside",6,2,4,[0,2]],["Outside",6,3,3[1,2]],["Outside",6,4,2,[2,2]],["Outside",6,5,1,[3,2]]]);
+
+astar.search([["Classroom 1",6,0,6,[0,0]],["Outside1",6,1,5, [0,1]],["Outside2",6,2,4,[0,2]],["Outside3",6,3,3,[1,2]],["Outside4",6,4,2,[2,2]],["Outside5",6,5,1,[3,2]]]);
