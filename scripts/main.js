fb1a5e29f5a466879ed1f151fa158df928abb297
Corey Buchan <xouwan021592@gmail.com>
2024-03-28 13:19:55 -0700

    Add more annotations. Change how aStar is initialized in main to make it consistent with the rest of the setup.

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,39 @@ import { ScreenPixelPosition, setupMap } from "./modules/map.js";
 import { userPosition, setupLocation, convertWorldToPercent } from "./modules/location.js";
 import { astar } from "./modules/pfNode.js";
 
+var aStar;
 function setupMainPage() {
   setupMap();
   setupLocation();
+  aStar = astar;
+  aStar.init;
+  console.log(aStar.search(
+    [
+      [
+        "Classroom 1", 3, 0, 3, [0, 0]
+      ],
+      [
+        "Outside1", 4, 1, 3, [0, 1]
+      ],
+      [
+        "Outside2", 3, 1, 2, [0, 2]
+      ],
+      ["Outside3", 3, 2, 1, [1, 2]
+      ],
+      [
+        "Outside4", 3, 2, 1, [2, 2]
+      ],
+      [
+        "Outside5", 3, 3, 0, [3, 2]
+      ]
+    ]
+  ));
 }
 
 document.addEventListener("DOMContentLoaded", setupMainPage);
-console.log(astar.search([["Classroom 1", 3, 0, 3, [0, 0]], ["Outside1", 4, 1, 3, [0, 1]], ["Outside2", 3, 1, 2, [0, 2]], ["Outside3", 3, 2, 1, [1, 2]], ["Outside4", 3, 2, 1, [2, 2]], ["Outside5", 3, 3, 0, [3, 2]]]));
+
+
+
 
 //we need to have an export to use an import, so this satisfies that requirement
 export { };
