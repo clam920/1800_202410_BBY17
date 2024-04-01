6b939bbad07a6a7eef7363856105c412c1be9e8d
Corey Buchan <xouwan021592@gmail.com>
2024-04-01 12:15:52 -0700

    add TODO to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -31,6 +31,14 @@ function setupMainPage() {
   ));
 }
 
+
+/**TODO
+ * 1) Event listener for User Click on Suggestion box INSIDE MAIN
+ * 1a) On click, query the database for the document with a matching **name** 
+ * 1b) get the **code** from the document
+ * 1c) pass the **code** to A* as the end location.
+ */
+
 document.addEventListener("DOMContentLoaded", setupMainPage);
 
 
